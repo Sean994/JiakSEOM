d27306978513d7008a6dc2f624d8319a8628d742
hjlg <huijialegerald@gmail.com>
2021-07-26 10:56:13 +0800

    wait

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
-
+const userController = require("../controllers/userController")
 router
   .route('/')
   .get(categoryController.getAllCategories)
   .post(categoryController.createCategory);
+
+router
+  .route('/user')
 
 module.exports = router;
