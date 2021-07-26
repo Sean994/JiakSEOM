2aad7d82d18f8cf7bb58461dd1bec2da44adcf71
Sean994 <yuanchen34@gmail.com>
2021-07-26 15:48:41 +0800

    completed postal code to address component, functional as standalone only

--- client/src/components/1_landing/Landing.jsx
+++ client/src/components/1_landing/Landing.jsx
@@ -1,20 +1,11 @@
+import PostalCode from "./PostalCode"
 const Landing = (props) => {
 
     return (
         <div>
             Landing page
-            <form>
-                <input 
-                className="postalCode"
-                type="text" 
-                placeholder="Enter your postal code"
-                id="postal code"
-                name="postal code"
-                />
-                <input 
-                type="submit" 
-                value="Delivery"/>
-            </form>
+            <PostalCode />
+         
         </div>
     )
 }
