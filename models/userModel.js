507fbd20adfe8ca3f3746d145423439a629401f9
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-07-26 16:46:45 +0800

    fix: 🐛 eslint fix on userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,53 +1,51 @@
 const mongoose = require('mongoose');
 
-
 const userSchema = new mongoose.Schema({
-  first_name : {
+  first_name: {
     type: String,
     trim: true,
   },
-  last_name : {
-      type : String,
-      trim : true,
+  last_name: {
+    type: String,
+    trim: true,
   },
-  contact : {
-      type : Number,
-      required : [true, 'Must enter contact number'],
-      trim : true,
+  contact: {
+    type: Number,
+    required: [true, 'Must enter contact number'],
+    trim: true,
   },
-  email : {
-      type : String,
-      required : [true, 'Must enter email'],
-      unique : [true, 'Email is already taken'],
-      trim : true,
+  email: {
+    type: String,
+    required: [true, 'Must enter email'],
+    unique: [true, 'Email is already taken'],
+    trim: true,
   },
-  username : {
-      type : String, 
-      required : [true, 'Enter username'],
-      unique : [true, 'Username is taken, please choose a different username'],
-      trim : true,
+  username: {
+    type: String,
+    required: [true, 'Enter username'],
+    unique: [true, 'Username is taken, please choose a different username'],
+    trim: true,
   },
-  password : {
-      type : String, 
-      required : [true, 'Enter password'],
-      trim : true,
+  password: {
+    type: String,
+    required: [true, 'Enter password'],
+    trim: true,
   },
-  address : {
-      type : String,
-      required : [true, 'Enter your address'],
-      trim : true,
+  address: {
+    type: String,
+    required: [true, 'Enter your address'],
+    trim: true,
   },
-  postal_code : {
-      type : Number, 
-      required : [true, 'Enter Postal Code'],
-      trim : true,
+  postal_code: {
+    type: Number,
+    required: [true, 'Enter Postal Code'],
+    trim: true,
   },
-  birthday : {
-      type : String,
-      trim : true,
-  }
+  birthday: {
+    type: String,
+    trim: true,
+  },
 });
-
 
 const User = mongoose.model('User', userSchema);
 
