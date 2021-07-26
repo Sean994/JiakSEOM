65fc0fb9c84bc949392abab6ece4e65e2d4a6587
Sean994 <yuanchen34@gmail.com>
2021-07-26 23:28:07 +0800

    tried login, encountered bug, will fix tmr. tried category, working

--- client/src/components/2_user/SignIn.jsx
+++ client/src/components/2_user/SignIn.jsx
@@ -1,41 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+const axios = require('axios').default;
 
 const SignIn = (props) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('/user/signin', {
+        username: event.target.username.value,
+        password: event.target.password.value,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    console.log(event.target.username.value);
+    console.log(event.target.password.value);
+  
+};
 
-    return (
-        <div>
-            <h1>User Sign In</h1>
-             <form>
-                <label htmlFor="username">
-                    Username: 
-                </label>
-                <input 
-                type="text" 
-                id="username"
-                name="username"
-                />
-                <br/>
-                <label htmlFor="password">
-                    Password: 
-                </label>
-                <input 
-                type="text" 
-                id="password"
-                name="password"
-                />
-                <br/>
-                <input 
-                type="submit" 
-                value="Sign In"/>
-                <br/>
-                <br/>
-                <span>New User? </span>
-                <Link to="/user/signup">
-                <button>Sign Up</button>
-                </Link>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>User Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" name="password" />
+        <br />
+        <input type="submit" value="Sign In" />
+        <br />
+        <br />
+        <span>New User? </span>
+        <Link to="/user/signup">
+          <button>Sign Up</button>
+        </Link>
+      </form>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
--- client/src/components/3_restaurants/Restaurants.jsx
+++ client/src/components/3_restaurants/Restaurants.jsx
@@ -1,8 +1,25 @@
+import { useState , useEffect } from "react";
 import { Link } from "react-router-dom"
+const axios = require('axios').default;
 
 const Restaurants = (props) => {
-const {clickHandle} = props
-const restaurantArray = ["Jeju", "Seoul", "Busan", "Daegu"]
+    const {clickHandle} = props
+    const [category, setCategory] = useState([])
+    const restaurantArray = ["Jeju", "Seoul", "Busan", "Daegu"]
+
+    useEffect(()=>{
+        axios.get('/api/v1/categories', {
+        })
+        .then(function (response) {
+          console.log(response)
+          setCategory(response.data.data.categories)
+          console.log(category)
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    },[])
+
     return (
         <div>
             <h1> List of Restaurants</h1>
@@ -17,6 +34,11 @@ const restaurantArray = ["Jeju", "Seoul", "Busan", "Daegu"]
                 </Link>
             ))}
             </ul>
+            <div>
+                {category.map((element,index) => (
+                    <p>{element.category_name}</p>   
+                ))}
+            </div>
         </div>
     )
 }
