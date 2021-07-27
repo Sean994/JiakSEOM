07e611517cf804b96e66da77c470354f471c73df
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-07-28 02:00:46 +0800

    feat: 🎸 category list basic set up

--- client/src/components/3_restaurants/Restaurants.jsx
+++ client/src/components/3_restaurants/Restaurants.jsx
@@ -1,45 +1,46 @@
-import { useState , useEffect } from "react";
-import { Link } from "react-router-dom"
-import CategoryRes from "./CategoryRes";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import CategoryRes from './CategoryRes';
 const axios = require('axios').default;
 
 const Restaurants = (props) => {
-    const {clickHandle} = props
-    const [categoryList, setCategoryList] = useState([])
-    const restaurantArray = ["Jeju", "Seoul", "Busan", "Daegu"]
+  const { clickHandle } = props;
+  const [categoryList, setCategoryList] = useState([]);
+  const restaurantArray = ['Jeju', 'Seoul', 'Busan', 'Daegu'];
 
-    useEffect(()=>{
-        axios.get('/api/v1/categories', {
-        })
-        .then(function (response) {
-          setCategoryList(response.data.data.categories)
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-    },[])
-  
-    return (
-        <div>
-            <h1> List of Restaurants</h1>
-            <ul>
-                {restaurantArray.map((element, index) => (
-                    <Link key={element} to={`/restaurants/${element}`}>
-                        <li key={element}
-                            onClick={() => clickHandle(element)}>
-                            {element} -
-                            index {index}
-                        </li>
-                    </Link>
-                ))}
-            </ul>
-            <div className="categoryList">
-                {categoryList.map((element,index) => (
-                    <CategoryRes keyid={index} catName={element.category_name}/> 
-                ))}
-            </div>
+  useEffect(() => {
+    axios
+      .get('/api/v1/categories', {})
+      .then(function (response) {
+        setCategoryList(response.data.data.categories);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1> List of Restaurants</h1>
+      <ul>
+        {restaurantArray.map((element, index) => (
+          <Link key={element} to={`/restaurants/${element}`}>
+            <li key={element} onClick={() => clickHandle(element)}>
+              {element} - index {index}
+            </li>
+          </Link>
+        ))}
+      </ul>
+      <div className="container">
+        <h3 className="text-body">Categories</h3>
+        <div className="container-xl d-flex overflow-auto align-baseline">
+          {categoryList.map((category) => (
+            <CategoryRes key={category._id} category={category} />
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Restaurants
+export default Restaurants;
