24248bc7adf04726ec5133d97299d38765b8f63b
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-07-28 00:31:57 +0800

    style: 💄 css for signin page

--- client/src/components/2_user/SignIn.jsx
+++ client/src/components/2_user/SignIn.jsx
@@ -25,45 +25,73 @@ const SignIn = (props) => {
   };
 
   return (
-    <div className="card" style={{ width: '30rem' }}>
-      <h1>User Sign In</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3 row">
-          <label htmlFor="username" className="col-sm-2 col-form-label">
-            Username:
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="form-control"
-            />
+    <div className="container  p-4 ">
+      {/* <div class="col-md-6 mx-auto text-center">
+        <div class="header-title">
+          <h1 class="wv-heading--title">Sign in</h1>
+        </div>
+      </div> */}
+      <div className="row ">
+        <div className="col-md-4 mx-auto border border-warning  border-1 p-4 rounded-3 shadow">
+          <div class="header-title text-center">
+            <h1 class="wv-heading--title">Sign in</h1>
+          </div>
+          <div className="myform form">
+            <form onSubmit={handleSubmit} className="container  p-4">
+              <div className="mb-3 row">
+                <div class="form-group">
+                  <label htmlFor="username" className="col-sm-4 col-form-label">
+                    Username:
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div class="form-group">
+                <label htmlFor="password" className="col-sm-4 col-form-label">
+                  Password:
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  className="form-control"
+                />
+              </div>
+
+              <div className="m-3 text-center">
+                <button
+                  type="submit"
+                  value="Sign In"
+                  className=" btn btn-block  btn btn-warning"
+                >
+                  Sign In
+                </button>
+              </div>
+              <div class="col-md-12 text-center my-3">
+                <div class="login-or">
+                  <hr class="hr-or" />
+                  <span class="span-or">New User?</span>
+                </div>
+              </div>
+              <div class="form-group text-center">
+                <Link to="/user/signup">
+                  <button className=" btn btn-block  btn btn-info">
+                    Sign Up
+                  </button>
+                </Link>
+              </div>
+              <p class="small mt-3">
+                By signing up, you will be one of our precious customers.
+              </p>
+            </form>
           </div>
         </div>
-        <div className="mb-3 row">
-          <label htmlFor="password" className="col-sm-2 col-form-label">
-            Password:
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              id="password"
-              name="password"
-              className="form-control"
-            />
-          </div>
-        </div>
-
-        <div className="mb-3 row">
-          <input type="submit" value="Sign In" />
-
-          <span>New User? </span>
-          <Link to="/user/signup">
-            <button>Sign Up</button>
-          </Link>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
