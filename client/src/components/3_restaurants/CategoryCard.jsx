5c69cf67b2f24b5574b1e4e212cf746d7d2abc55
Sean994 <yuanchen34@gmail.com>
2021-07-28 13:12:33 +0800

    Filter restaurant Cards by category_id

--- client/src/components/3_restaurants/CategoryCard.jsx
+++ client/src/components/3_restaurants/CategoryCard.jsx
@@ -1,9 +1,25 @@
-const CategoryCard = ({ category }) => {
+import axios from 'axios';
+const CategoryCard = (props) => {
+  const { category, setRestaurantList } = props;
+
+  const filterCatRest = (id) => {
+    axios.get(` /api/v1/restaurants?&category=${id}`).then((res) => {
+      console.log(id);
+      console.log(res.data.data.restaurants);
+      setRestaurantList(res.data.data.restaurants)
+    });
+
+    // axios.get(`/api/v1/categories/${id}/restaurants`).then((res) => {
+    //   console.log(id);
+    //   console.log(res.data.restaurants);
+    // });
+  };
   return (
     <div
       id={category._id}
       className="card me-2 shadow-sm border-0"
       style={{ minWidth: '12rem' }}
+      onClick={() => filterCatRest(category._id)}
     >
       <img
         src={category.img_cover}
--- client/src/components/3_restaurants/Restaurants.jsx
+++ client/src/components/3_restaurants/Restaurants.jsx
@@ -1,14 +1,11 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import CategoryCard from './CategoryCard';
 import RestaurantCard from './RestaurantCard';
 import axios from 'axios';
 
 const Restaurants = (props) => {
-  const { clickHandle } = props;
   const [categoryList, setCategoryList] = useState([]);
   const [restaurantList, setRestaurantList] = useState([]);
-  const restaurantArray = ['Jeju', 'Seoul', 'Busan', 'Daegu'];
 
   useEffect(() => {
     axios
@@ -22,7 +19,7 @@ const Restaurants = (props) => {
     axios
       .get('/api/v1/restaurants', {})
       .then((res) => {
-        console.log(res.data.data)
+        console.log(res.data.data.restaurants)
         setRestaurantList(res.data.data.restaurants);
       })
       .catch((err) => {
@@ -32,31 +29,26 @@ const Restaurants = (props) => {
 
   return (
     <div>
-      <h1> List of Restaurants</h1>
-      <ul>
-        {restaurantArray.map((element, index) => (
-          <Link key={element} to={`/restaurants/${element}`}>
-            <li key={element} onClick={() => clickHandle(element)}>
-              {element} - index {index}
-            </li>
-          </Link>
-        ))}
-      </ul>
+
 
       <div className="container">
         <h3 className="text-body">Categories</h3>
         <div className="container-xl d-flex overflow-auto align-baseline">
           {categoryList.map((category) => (
-            <CategoryCard key={category._id} category={category} />
+            <CategoryCard key={category._id} category={category} setRestaurantList={setRestaurantList} />
           ))}
         </div>
       </div>
 
       <div className="container">
         <h3 className="text-body">All Restaurants</h3>
-        <div className="container-xl restContainer">
+        <div className="container-xl restContainer row">
           {restaurantList.map((restaurant) => (
-            <RestaurantCard key={restaurant._id} restaurant={restaurant} />
+            < div className="col-4 mb-4"
+            >
+            <RestaurantCard key={restaurant._id} restaurant={restaurant}
+             />
+            </div>
           ))}
         </div>
       </div>
