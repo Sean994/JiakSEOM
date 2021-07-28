32e63435930318c35c6982c2c68b84cf186ef214
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-07-28 15:36:55 +0800

    feat: 🎸 create checkoutpage + order side bar

--- client/src/components/5_order/CartItem.jsx
+++ client/src/components/5_order/CartItem.jsx
@@ -1,28 +1,61 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
 
 const CartItem = () => {
+  // item from props
+  const originPrice = 24; // default will be item.price props = item item.price
+
+  const [quantity, setQuantity] = useState(1);
+
+  //! props.function
+  const [price, setPrice] = useState(originPrice); // this will be origin price * quantity
+
+  useEffect(() => {
+    setPrice((prev) => originPrice * quantity);
+
+    //! props.function  to send up the info.
+  }, [quantity]);
+
+  const plusHandler = () => {
+    setQuantity((prev) => prev + 1);
+    setPrice((prev) => originPrice);
+  };
+
+  const minusHandler = () => {
+    setQuantity((prev) => prev - 1);
+  };
+
   return (
-    <div className="bg-light row p-2 d-flex flew-row align-item-center ">
-      <div className="col-8 d-flex flew-row align-item-center justify-content-start">
+    <div className="bg-white row d-flex flew-row align-item-center mb-2">
+      <div className="col-8 d-flex flew-row align-items-center justify-content-start p-0">
         <img
-          className="img-thumbnail me-2"
-          style={{ width: '4rem' }}
+          className="img-thumbnail me-1"
+          style={{ width: '4rem', height: '100%' }}
           src="https://recipe1.ezmember.co.kr/cache/recipe/2015/11/03/ca654152bb5ed8a9521ec901ab5c34211.jpg"
           alt="item"
         />
         <div className="half">Mega Max Chicken Meal</div>
       </div>
 
-      <div className="col-3 offset-md-1 align-self-end">
-        <strong className="right">
-          S$ <span>24.21</span>
-        </strong>
-        <div className="d-flex">
-          <button type="button" className="btn btn-light">
-            <FontAwesomeIcon size="sm" icon={['fas', 'trash-alt']} />
+      <div className="col-3 offset-md-1 align-self-end px-1 ">
+        <div className=" text-end me-1">
+          S$ <span id="itemPrice">{price}</span>
+        </div>
+        <div className="d-flex ">
+          <button
+            type="button"
+            className="btn btn-white"
+            onClick={minusHandler}
+          >
+            {quantity === 1 && (
+              <FontAwesomeIcon size="sm" icon={['fas', 'trash-alt']} />
+            )}
+            {quantity > 1 && (
+              <FontAwesomeIcon size="sm" icon={['fas', 'minus']} />
+            )}
           </button>
-          <span className="p-1">1</span>
-          <button type="button" className="btn btn-light">
+          <span className="p-1">{quantity}</span>
+          <button type="button" className="btn btn-white" onClick={plusHandler}>
             <FontAwesomeIcon size="sm" icon={['fas', 'plus']} />
           </button>
         </div>
