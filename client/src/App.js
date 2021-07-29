49fdf81927653ecaca7b1a3b6be953525c00dbaa
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-07-29 17:40:32 +0800

    feat: 🎸 middle way of auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import {
+  Redirect,
+  Route,
+  Switch,
+  useLocation,
+  useHistory,
+} from 'react-router-dom';
 import NavBar from './components/0_NavBar';
 import Landing from './components/1_landing/Landing';
 import SignIn from './components/2_user/SignIn';
@@ -11,6 +17,74 @@ import Restaurants from './components/3_restaurants/Restaurants';
 import CheckOut from './components/4_checkout/Checkout.jsx';
 import Reviews from './components/6_review/Reviews';
 import './styles/style.css';
+
+const Auth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+};
+
+function AuthButton() {
+  const history = useHistory();
+  return Auth.isAuthenticated ? (
+    <p>
+      welcome{' '}
+      <button onClick={() => Auth.signout(() => history.push('/'))}>
+        signout
+      </button>{' '}
+    </p>
+  ) : (
+    <p></p>
+  );
+}
+
+function Login() {
+  const [redirectToReferrer, setRedirectToReferrer] = useState(false);
+
+  const login = () => {
+    Auth.authenticate(() => {
+      setRedirectToReferrer(true);
+    });
+  };
+  const { state } = useLocation();
+
+  if (redirectToReferrer === true) {
+    return <Redirect to={state?.from || '/'} />;
+  }
+
+  return (
+    <div>
+      <p>You must log in to view this page</p>
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return Auth.isAuthenticated === true ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    ></Route>
+  );
+}
 
 function App() {
   const [restaurant, setRestaurant] = useState({});
@@ -46,6 +120,7 @@ function App() {
         order={order}
       />
       <main>
+        <AuthButton />
         <Switch>
           <Route exact path="/">
             <Redirect to="/landing" />
@@ -84,12 +159,18 @@ function App() {
           </Route>
           {/* if cookie with the session id => (loggedin) => link to checkout 
           if no cookie with the session id => link to login page */}
+
           <Route path="/checkout">
             <CheckOut address={address} order={order} restaurant={restaurant} />
           </Route>
           <Route path="/review">
             <Reviews user={user} />
           </Route>
+
+          <PrivateRoute path="/protected">
+            <Reviews user={user} />
+          </PrivateRoute>
+
           <Redirect from="*" to="/" />
         </Switch>
       </main>
